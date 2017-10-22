852f81fa54f9dd65a83ccc8f64c118e5ec16d6ab
Nader Albert <nadera@worldmanager.com>
2017-10-22 20:07:48 +1100

    introduced a `Distance` model

--- src/main/scala/na/weatherballoon/BalloonLog.scala
+++ src/main/scala/na/weatherballoon/BalloonLog.scala
@@ -1,12 +1,8 @@
 package na.weatherballoon
 
-/**
- * @author nader albert
- * @since  11/11/2015.
- */
-//<timestamp>|<location>|<temperature>|<observatory>
-case class Location(x: Int, y: Int)
-case class Temperature(value: Int, unit: String)
+case class Location(x: Double, y: Double)
+case class Temperature(value: Double, unit :String)
+case class Distance(value: Double, unit :String)
 case class Observatory(code: String) //AU, US, FR, or anything else
 
 case class BalloonLog ()
