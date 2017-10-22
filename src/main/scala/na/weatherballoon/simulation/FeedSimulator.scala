2aa486b32fcc0ceaaf3c14d5157f6dd2871b49c3
Nader Albert <nadera@worldmanager.com>
2017-10-22 20:06:39 +1100

    generate random data for balloon coordinates

--- src/main/scala/na/weatherballoon/simulation/FeedSimulator.scala
+++ src/main/scala/na/weatherballoon/simulation/FeedSimulator.scala
@@ -6,12 +6,9 @@ import scala.util.Random
 
 object FeedSimulator {
 
-    val timeGenerator        = new TimeGenerator
-    val temperatureGenerator = new TemperatureGenerator
-    val observatoryGenerator = new ObservatoryGenerator
-
     def nextRecord: String = {
-        timeGenerator.generate + "|10,5|" + temperatureGenerator.generate + "|" + observatoryGenerator.generate
+        val location = LocationGenerator.generate
+        TimeGenerator.generate + "|" +  location._1 + "," + location._2 + "|" + TemperatureGenerator.generate + "|" + ObservatoryGenerator.generate
     }
 }
 
@@ -19,48 +16,57 @@ trait Generator[T]{
     def generate: T
 }
 
-class TemperatureGenerator extends Generator[Int] {
+object TemperatureGenerator extends Generator[Int] {
     def generate: Int = Random.nextInt(500)
 }
 
-class ObservatoryGenerator extends Generator[String] {
+object LocationGenerator extends Generator[(Double, Double)] {
+    def generate: (Double, Double) = (Random.nextDouble + Random.nextInt(5), Random.nextDouble + Random.nextInt(1000))
+}
+
+object ObservatoryGenerator extends Generator[String] {
     import Observatories._
     import DistanceUnits._
     import TemperatureUnits._
 
     val observatoryCodes: Map[String, (String, String)] =
         Map.empty[String, (String, String)]
-            .withDefault(_ => (Kelvin, KM))
-            .updated(AU, (Celsius, KM))
-            .updated(US, (Fahrenheit, Miles))
-            .updated(FR, (Kelvin, Meter))
+            .withDefault(_ =>       (Kelvin, KILOMETERS))
+            .updated(AUSTRALIA,     (Celsius, KILOMETERS))
+            .updated(UNITED_STATES, (Fahrenheit, Miles))
+            .updated(FRANCE,        (Kelvin, Meter))
+            .updated(FRANCE,        (Kelvin, Meter))
+
 
     def generate: String = {
         observatoryCodes.keys.drop(Random.nextInt(observatoryCodes.keys.size)).head
     }
 }
 
-class TimeGenerator extends Generator [LocalDateTime] {
+object TimeGenerator extends Generator [LocalDateTime] {
     // Assuming that all simulation data will be on the same year and month, and spread on a range of two consecutive days
     override def generate: LocalDateTime = LocalDateTime.of(2017, 2, Random.nextInt(2) + 1, Random.nextInt(24), Random.nextInt(60))
 }
 
 object Observatories {
-    val AU = "AU"
-    val US = "US"
-    val FR = "FR"
-    val IT = "IT"
+    val AUSTRALIA       = "AU"
+    val UNITED_STATES   = "US"
+    val FRANCE          = "FR"
+    val ITALY           = "IT"
+    val ECUADOR         = "EC"
+    val BOLIVIA         = "BL"
+    val BRAZIL          = "BR"
 }
 
 object TemperatureUnits {
-    val Kelvin     = "kelvin"
+    val Kelvin     = "kv"
     val Celsius    = "celsius"
     val Fahrenheit = "fahrenheit"
 }
 
 object DistanceUnits {
-    val KM     = "km"
-    val Miles  = "miles"
-    val Meter  = "m"
+    val KILOMETERS  = "km"
+    val Miles       = "miles"
+    val Meter       = "m"
 
 }
