3b1cbea674ae1f6de88283e70cab4339572ad45b
Nader Albert <nadera@worldmanager.com>
2017-10-23 21:40:05 +1100

    added Print command

--- src/main/scala/na/weatherballoon/BalloonLog.scala
+++ src/main/scala/na/weatherballoon/BalloonLog.scala
@@ -5,4 +5,4 @@ case class Temperature(value: Double, unit :String)
 case class Distance(value: Double, unit :String)
 case class Observatory(code: String) //AU, US, FR, or anything else
 
-case class BalloonLog ()
+case object PrintOutput
