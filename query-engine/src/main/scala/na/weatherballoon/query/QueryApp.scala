36c7c349df21553510794ff0bfe725ad20624414
Nader Albert <nadera@worldmanager.com>
2017-10-25 20:52:21 +1100

    implemented a regular expression to match different commands

--- query-engine/src/main/scala/na/weatherballoon/query/QueryApp.scala
+++ query-engine/src/main/scala/na/weatherballoon/query/QueryApp.scala
@@ -9,45 +9,32 @@ object QueryApp extends App {
 
     val system = ActorSystem("QueryEngine")
 
-    printWelcomeMessage()
-
     //TODO: Name of the Analysis Engine should be retrieved from application configuration
     val aggregator = system.actorSelection("akka.tcp://AnalysisEngine@127.0.0.1:2552/user/publisher-guardian")
+
+    printWelcomeMessage()
 
     /**
       **/
     private def parse(command: String): Unit = {
         command match {
-            case Statistics.MaxTemp =>
-                aggregator ! PrintOutput(Seq.empty[String].+:(Statistics.MaxTemp))
+            case statistics: String if statistics.
+                matches(
+                    "(" +
+                        "(\\|?" + Statistics.MaxTemp                        + ")" + "|" +
+                        "(\\|?" + Statistics.MinTemp                        + ")" + "|" +
+                        "(\\|?" + Statistics.MeanTemp                       + ")" + "|" +
+                        "(\\|?" + Statistics.OBSERVATIONS_PER_OBSERVATORY   + ")" + "|" +
+                        "(\\|?" + Statistics.TOTAL_NUMBER_OF_OBSERVATIONS   + ")" + "|" +
+                        "(\\|?" + Statistics.TOTAL_DISTANCE                 + ")"
+                    + ")*"
+                ) =>
+
+                aggregator ! PrintOutput(statistics.split("\\|").toSeq)
                 parse(StdIn.readLine)
 
-            case Statistics.MinTemp =>
-                aggregator ! PrintOutput(Seq.empty[String].+:(Statistics.MinTemp))
-                parse(StdIn.readLine)
-
-            case Statistics.MeanTemp =>
-                aggregator ! PrintOutput(Seq.empty[String].+:(Statistics.MeanTemp))
-                parse(StdIn.readLine)
-
-            case Statistics.TOTAL_DISTANCE =>
-                aggregator ! PrintOutput(Seq.empty[String].+:(Statistics.TOTAL_DISTANCE))
-                parse(StdIn.readLine)
-
-            case Statistics.OBSERVATIONS_PER_OBSERVATORY =>
-                aggregator ! PrintOutput(Seq.empty[String].+:(Statistics.OBSERVATIONS_PER_OBSERVATORY))
-                parse(StdIn.readLine)
-
-            case Statistics.TOTAL_NUMBER_OF_OBSERVATIONS =>
-                aggregator ! PrintOutput(Seq.empty[String].+:(Statistics.TOTAL_NUMBER_OF_OBSERVATIONS))
-                parse(StdIn.readLine)
-
-            case statistics: String if statistics.matches("(?:" + Statistics.MaxTemp + " | " + Statistics.MinTemp + ")") =>
-                aggregator ! PrintOutput(statistics.split("-").toSeq)
-                parse(StdIn.readLine)
-
-            case "Quit" =>
-                println("exiting ......")
+            case "quit" =>
+                println("exiting ...........")
                 System.exit(1)
 
             case _ =>
@@ -59,9 +46,9 @@ object QueryApp extends App {
     private def printWelcomeMessage(): Unit = {
         println("\n ********************************************************************************** \n ")
 
-        println("Enter one or more commands from the list below: - NB: Multiple commands must be separated by `-`. \n ")
+        println("Enter one or more commands from the list below: - NB: Multiple commands must be separated by `|`. \n ")
 
-        println("Example: MaxTemp - MinTemp - \n ")
+        println("Example: MaxTemp|MinTemp|TotalDist \n ")
 
         println(
             " [" + "        " + Statistics.MaxTemp + "         "+ "] :: calculates the maximum temperature that have been recorded so far "          + "\n" +
@@ -70,7 +57,7 @@ object QueryApp extends App {
             " [" + "        " + Statistics.TOTAL_DISTANCE+"       "+ "] :: calculates the total distance travelled so far"                              + "\n" +
             " [" + "       " + Statistics.OBSERVATIONS_PER_OBSERVATORY + "      " + "] :: calculates the number of observations recorded per each observatory so far " + "\n" +
             " [" + "       " + Statistics.TOTAL_NUMBER_OF_OBSERVATIONS + "     " + "] :: calculates the total number of observations recorded so far"                 + "\n" +
-            " [" + "Quit" + " ] :: to exit query app")
+            " [" + "          Quit" + "          ] :: to exit query app")
 
         parse(StdIn.readLine)
     }
